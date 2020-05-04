31216f632115ca995aced4ffc3d6b8cb03870706
Reshinth Adithyan <36307201+ReshinthER@users.noreply.github.com>
2020-05-04 23:30:40 +0530

    Add files via upload

---
+++ Test Folder/Mod_Test_Input.py
@@ -0,0 +1,10 @@
+import b
+import impfunc
+def Funca():
+    Out = Funcb()
+    A = impfunc()
+    a = 'b'
+    None
+def Funcb():
+    print('Happy')
+Output = Func1 
---
+++ Test Folder/Test_Input.py
@@ -0,0 +1,10 @@
+import a 
+#import impfunc
+def Func1():
+    Out = Func2()
+    A = impfunc()
+    a = 'b'
+    None
+def Func2():
+    print('Happy')
+Output = Func1 
